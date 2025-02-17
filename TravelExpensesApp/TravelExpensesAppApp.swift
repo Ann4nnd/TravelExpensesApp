4624779804e87547eaa670ed612a3e753615b126
Анна <anna@MacBook-Pro-Anna.local>
2025-02-17 11:13:17 +0300

    Initial commit

---
+++ TravelExpensesApp/TravelExpensesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelExpensesAppApp.swift
+//  TravelExpensesApp
+//
+//  Created by Анна on 17.02.25.
+//
+
+import SwiftUI
+
+@main
+struct TravelExpensesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
